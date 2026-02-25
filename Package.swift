4d01950121909fc50643c47239508107d7b205f1
Flytant <flytant@gmail.com>
2026-02-25 12:34:28 +0530

    Update binary URL and checksum for v1.0.0

--- Package.swift
+++ Package.swift
@@ -14,14 +14,13 @@ let package = Package(
         .iOS(.v15)
     ],
     products: [
-        .library(
-            name: "PopbrainAttributionSDK",
-            targets: ["PopbrainAttributionSDK"]),
+        .library(name: "PopbrainAttributionSDK", targets: ["PopbrainAttributionSDK"])
     ],
     targets: [
-        .target(
+        .binaryTarget(
             name: "PopbrainAttributionSDK",
-            path: "PopbrainAttributionSDK.xcframework" // This must match the folder name where your .swift files are
+            url: "https://github.com/AnilFlytant/PopbrainAttributionSDK/releases/download/v1.0.0/PopbrainAttributionSDK.zip",
+            checksum: "36cca9c121172ca5a07bc8f408bf3cdff6d994d2a6bd40ab6e294e98e9dc081e"
         )
     ]
 )
